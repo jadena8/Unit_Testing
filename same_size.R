e150451405ae8d917cc5e9d877ef7f4cff35941b
Jaden Anderson <jadena@ku.edu>
2018-04-12 21:43:22 -0500

    Add starting files, Proposal.rmd, Tests.Rmd, and 1st empty function same_size.R

---
+++ same_size.R
@@ -0,0 +1,12 @@
+#This function is to take a multiple sequence alignment (MSAs) and determine if they are the same size. 
+#The sequences need to be the same size in order to generate a square matrix for calculation.
+#
+#Input: msa <- a multiple sequence alignment read in from a file
+#
+#same_size Function: Check to see if all sequences in the file are the same length
+#
+#
+
+same_size <- function(msa){
+  
+} 
