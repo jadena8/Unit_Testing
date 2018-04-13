61ed96feb6c3fee3b86a23d4ceebf4431b92119f
Jaden Anderson <jadena@ku.edu>
2018-04-13 01:28:35 -0500

    Halfway point

--- is_protein.R
+++ is_protein.R
@@ -1,1 +1,13 @@
-#Is Protein?
+#This function takes in a msa to check whether or not the sequences have large gaps.
+#If the sequences do have large gaps, then they will fail the unit test
+#
+#Input: msa <- a multiple sequence alignment read in from a file or pasted, MUST NOT BE IN TRADITIONAL FORMATS, sequences only
+#Output: A PASS or FAIL to see if the sequences passed or failed the unit test
+#           if failed: the code will also print out the sequence number that failed the unit test
+#
+#is_protein function: counts the number of gaps in the msa vector, if greater than 10 for any sequence fails unit test
+#
+
+is_protein <- function(msa) {
+  
+}
--- test_calc.R
+++ test_calc.R
@@ -1,1 +1,13 @@
-#test calculation of dN/dS
+#This function takes in a msa to check whether or not the sequences are in a FASTA format.
+#If the sequences are in FASTA format, they will pass, if not they will fail the unit test
+#
+#Input: msa <- a multiple sequence alignment read in from a file
+#Output: A PASS or FAIL to see if the sequences passed or failed the unit test
+#           if failed: the code will also print out the sequence number that failed the unit test
+#
+#test_calc function: check to see if the sequences are in FASTA format since I customized the alignment. Will check for > on the first line and for sequences on the next.
+#
+
+test_calc <- function(msa) {
+  
+}
